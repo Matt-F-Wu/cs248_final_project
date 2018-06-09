e5fef887c8c434e5e881815db7985199dcec48fb
Hao Wang <haowang.me@gmail.com>
2018-06-08 21:11:01 -0700

    Some trials.

--- js/particle.js
+++ js/particle.js
@@ -38,25 +38,26 @@ var camera, tick = 0,
       Position is the position of the new particle.
       We can change the position every time we spawn
       instead of moving particles around, we can simply
-      let the old ones die (lifetime), and draw new ones 
+      let the old ones die (lifetime), and draw new ones
       at new positions to give the illusion that particles are moving
       */
 
       options = {
-        position: new THREE.Vector3(),
+        position: new THREE.Vector3(-20, 0, 0),
         positionRandomness: .3,
-        velocity: new THREE.Vector3(),
-        velocityRandomness: .5,
-        color: 0xaa88ff,
-        colorRandomness: .2,
-        turbulence: .5,
-        lifetime: 2,
-        size: 15,
+        velocity: new THREE.Vector3(10, 0, 0),
+        velocityRandomness: 1,
+        // TODO 加一个向心加速度，然后根据 position、velocity 和当前位置来确定加速度方向
+        color: 0x000000,
+        colorRandomness: 1,
+        turbulence: 0,
+        lifetime: 20,
+        size: 8,
         sizeRandomness: 1
       };
 
       spawnerOptions = {
-        spawnRate: 15000,
+        spawnRate: 2000,
         horizontalSpeed: 1.5,
         verticalSpeed: 1.33,
         timeScale: 1
@@ -67,7 +68,7 @@ var camera, tick = 0,
       renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
       renderer.setPixelRatio( window.devicePixelRatio );
       renderer.setSize( container.clientWidth, container.clientHeight );
-      renderer.setClearColor(0x000000, 0.0);
+      renderer.setClearColor(0x000000, 1.0); // TODO: 1.0->0.0
       container.appendChild( renderer.domElement );
 
       window.addEventListener( 'resize', onWindowResize, false );
@@ -96,7 +97,8 @@ var camera, tick = 0,
 
       if ( delta > 0 ) {
 
-        options.position.multiplyScalar(Math.random()*4.0 - 2.0);
+        offset = 0
+        options.position.addScalar(Math.random() * offset- offset / 2);
 
         for ( var x = 0; x < spawnerOptions.spawnRate * delta; x++ ) {
 
